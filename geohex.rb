dd3ebce85726cf3ab74a7009a5bdc075df05be5f
treyking11 <treyking.dev@gmail.com>
2017-04-06 21:29:41 -0700

    another commit

--- geohex.rb
+++ geohex.rb
@@ -27,7 +27,9 @@ end
 
 post '/upload' do
   @content = CSV.read('look_back_addresses.csv').to_s
-  erb :index, layout: :main
+  # erb :index, layout: :main
+  # csv_input_data = CSV.read($filename)
+  # return csv_input_data
   return @content
 end
 
